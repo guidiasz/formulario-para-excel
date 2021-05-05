7dfd56d305f8a67c6c7d0e749622d4914aa48d7a
Guilherme Dias <gui.diasz@hotmail.com>
2021-05-05 10:43:37 -0300

    Correção no nome da variável uri

--- conexao/conexao.js
+++ conexao/conexao.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
 const URI = process.env.URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
